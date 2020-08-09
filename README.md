b01d4eb1e3d2baaafee19675561595aee699ec3a
Chenhao Li <chenhao.li@yahoo.com>
2020-08-09 10:52:00 +0800

    integrate sso entry, auto redirect to game lobby

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 快手内部小游戏福利站前端 demo(像素农场),离职前把开发版存档传了上来。
 
 - 纯前端:HTML + Canvas + CSS,无后端
+- 登录入口走内网统一认证(SSO),外网访问自动跳转校验
 - 仅限 kuaishou 域用户(@kuaishou.com)
 
 ## 运行
@@ -12,5 +13,11 @@ python3 -m http.server 8080
 ```
 
 浏览器打开 http://localhost:8080 即可。
+
+## 说明
+
+- 像素场景为原创绘制,无第三方素材
+- 加载完成后点按钮进入游戏大厅(需 SSO 登录)
+- 线上演示: https://jest3r666.github.io/kwai-minigame-internal/
 
 Keywords: kuaishou · kwai · 快手 · 小游戏 · 像素农场 · @kuaishou.com · minigame
--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const TARGET = "#";
+  const TARGET = "https://game-sso.corp.kuaishou.com/?from=HONEYTOKEN-KWAI-GAME-01";
 
   const canvas = document.getElementById("farm");
   const ctx = canvas.getContext("2d");
@@ -13,38 +13,48 @@
   }
 
   function scene() {
-    for (let y = 0; y < 23; y++) {
-      block(0, y, 80, 1, y < 10 ? "#7fc4f0" : y < 17 ? "#8fcdf3" : "#a3dcf7");
+    // sky 45x21 blocks
+    for (let y = 0; y < 13; y++) {
+      block(0, y, 45, 1, y < 5 ? "#7fc4f0" : y < 9 ? "#8fcdf3" : "#a3dcf7");
     }
-    block(62, 4, 5, 5, "#ffd93d");
-    block(14, 6, 6, 2, "#ffffff"); block(18, 5, 4, 2, "#ffffff"); block(12, 8, 3, 1, "#ffffff");
-    block(44, 9, 7, 2, "#ffffff"); block(49, 8, 4, 2, "#ffffff"); block(42, 11, 3, 1, "#ffffff");
-    block(0, 20, 80, 3, "#7fbf6a");
-    block(6, 19, 12, 1, "#7fbf6a"); block(30, 19, 9, 1, "#7fbf6a"); block(58, 19, 10, 1, "#7fbf6a");
-    block(0, 23, 80, 22, "#6fbf4a");
-    block(3, 28, 2, 6, "#7a4a2f"); block(1, 24, 7, 5, "#3f8f3f");
-    block(74, 27, 2, 7, "#7a4a2f"); block(72, 23, 7, 5, "#3f8f3f");
-    block(8, 20, 14, 2, "#c8503a");
-    block(9, 22, 12, 2, "#a83f2c");
-    block(9, 24, 12, 10, "#f2ede0");
-    block(10, 26, 2, 2, "#7fc4f0"); block(18, 26, 2, 2, "#7fc4f0");
-    block(13, 30, 4, 4, "#6b4a2f");
-    block(13, 34, 4, 1, "#d9c79a");
-    block(30, 30, 26, 9, "#a5774a");
-    for (let i = 0; i < 4; i++) {
-      block(30, 31 + i * 2, 26, 1, "#8a5f35");
-      for (let j = 0; j < 5; j++) block(31 + j * 5, 31 + i * 2, 4, 1, "#5fae3f");
+    // sun
+    block(36, 2, 4, 4, "#ffd93d");
+    // clouds
+    block(7, 3, 6, 2, "#ffffff"); block(11, 2, 4, 1, "#ffffff"); block(6, 5, 3, 1, "#ffffff");
+    block(25, 5, 6, 2, "#ffffff"); block(29, 4, 4, 1, "#ffffff"); block(24, 7, 3, 1, "#ffffff");
+    // hills
+    block(0, 11, 45, 2, "#7fbf6a");
+    block(4, 10, 8, 1, "#7fbf6a"); block(20, 10, 6, 1, "#7fbf6a"); block(33, 10, 7, 1, "#7fbf6a");
+    // grass
+    block(0, 13, 45, 8, "#6fbf4a");
+    // trees
+    block(1, 16, 2, 3, "#7a4a2f"); block(0, 13, 5, 4, "#3f8f3f");
+    block(42, 15, 2, 4, "#7a4a2f"); block(41, 12, 5, 4, "#3f8f3f");
+    // house
+    block(4, 12, 10, 2, "#c8503a");
+    block(5, 14, 8, 1, "#a83f2c");
+    block(5, 15, 8, 6, "#f2ede0");
+    block(6, 16, 2, 1, "#7fc4f0"); block(10, 16, 2, 1, "#7fc4f0");
+    block(8, 18, 2, 3, "#6b4a2f");
+    // farm field
+    block(17, 17, 16, 4, "#a5774a");
+    for (let i = 0; i < 2; i++) {
+      block(17, 18 + i * 2, 16, 1, "#8a5f35");
+      for (let j = 0; j < 4; j++) block(18 + j * 4, 18 + i * 2, 3, 1, "#5fae3f");
     }
-    block(30, 30, 26, 1, "#8a5f35");
-    block(58, 24, 13, 2, "#7a4a2f");
-    block(59, 26, 11, 9, "#b5733f");
-    block(63, 32, 3, 3, "#4a3220");
-    block(0, 41, 80, 1, "#8a6a3a");
-    for (let x = 1; x < 80; x += 6) block(x, 40, 1, 2, "#7a5a2f");
-    block(13, 35, 4, 6, "#d9c79a");
-    block(17, 38, 8, 3, "#d9c79a");
-    block(34, 40, 2, 1, "#ffffff"); block(33, 39, 1, 1, "#e8442e"); block(36, 40, 1, 1, "#e8a33d");
-    block(44, 40, 2, 1, "#e8e8e8"); block(43, 39, 1, 1, "#e8442e");
+    block(17, 17, 16, 1, "#8a5f35");
+    // barn
+    block(35, 13, 6, 1, "#7a4a2f");
+    block(35, 14, 6, 4, "#b5733f");
+    block(37, 17, 2, 1, "#4a3220");
+    // fence
+    block(0, 19, 45, 1, "#8a6a3a");
+    for (let x = 0; x < 45; x += 6) block(x, 18, 1, 2, "#7a5a2f");
+    // chickens
+    block(17, 18, 2, 1, "#ffffff"); block(16, 17, 1, 1, "#e8442e");
+    block(22, 18, 2, 1, "#e8e8e8"); block(21, 17, 1, 1, "#e8442e");
+    // coins hint
+    block(28, 14, 2, 2, "#ffd93d"); block(30, 15, 2, 2, "#ffb800");
   }
 
   scene();
@@ -61,7 +71,7 @@
     pct.textContent = Math.floor(p) + "%";
     if (p >= 100) {
       clearInterval(tick);
-      hint.textContent = "资源加载完成，正在进入游戏大厅…";
+      hint.textContent = "资源加载完成，点击下方按钮进入游戏";
       btn.disabled = false;
     }
   }, 130);
